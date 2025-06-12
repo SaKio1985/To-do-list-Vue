5d023162e15c05b6e03e37dd9840244bce3a7a34
SaKio1985 <116895163+SaKio1985@users.noreply.github.com>
2025-06-12 19:35:06 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vue-todo-app/', // Reemplaza con el nombre de tu repositorio
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
